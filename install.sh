cda4b040752d2beb033593436ffe619811adab8e
Anatoli <64985361+DaDe287@users.noreply.github.com>
2025-10-05 14:06:04 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+clear
+sudo apt update && sudo apt upgrade -y
+
+bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/MySQL_Admin/main/install-mysql.sh)
+
+bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/MySQL_Admin/main/install-phpmyadmin.sh)
+
+clear
