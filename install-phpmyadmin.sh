24d03ff9f0f34f63be3de36dcbe4ac9774a04a1f
Anatoli <64985361+DaDe287@users.noreply.github.com>
2025-10-11 01:06:27 +0300

    Update install-phpmyadmin.sh

--- install-phpmyadmin.sh
+++ install-phpmyadmin.sh
@@ -1,5 +1,8 @@
 clear
-sudo apt install apache2 -y
 sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
-sudo phpenmod mbstring 
+sudo phpenmod mbstring
+sudo apt install php libapache2-mod-php
+sudo a2enmod php8.3
+sudo a2enmod ssl
+sudo a2enmod rewrite
 sudo systemctl restart apache2 
